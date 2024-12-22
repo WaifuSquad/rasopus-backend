b8ca5bfe275d234ff0fd4e6f83b381eca571fd1b
Torben Schweren <torben@schweren.dev>
2024-12-22 01:45:52 +0100

    add: OpenAPI/Swagger support

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,59 @@
-use rocket::{get, routes, Rocket};
+use rocket::{
+    get,
+    serde::{json::Json, Deserialize, Serialize},
+    Rocket,
+};
+use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*, JsonSchema};
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Serialize, Deserialize, JsonSchema)]
+#[serde(crate = "rocket::serde", rename_all = "camelCase")]
+#[schemars(crate = "okapi::schemars")]
+pub enum TestEnum {
+    FirstOption(i8),
+    SecondOption(i16),
+    IdkHowThisWillLookInOpenApi(i32, u128),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Serialize, Deserialize, JsonSchema)]
+#[serde(crate = "rocket::serde", rename_all = "camelCase")]
+#[schemars(crate = "okapi::schemars")]
+pub struct TestStruct {
+    some_number: i8,
+    another_number: i16,
+    you_get_it: i32,
+}
+
+#[openapi]
+#[get("/test_enum")]
+fn test_enum() -> Json<TestEnum> {
+    Json(TestEnum::FirstOption(1))
+}
+
+#[openapi]
+#[get("/test_struct")]
+fn test_struct() -> Json<TestStruct> {
+    Json(TestStruct {
+        some_number: 1,
+        another_number: 2,
+        you_get_it: 3,
+    })
+}
+
+#[openapi()]
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/num/<num>")]
-fn num_u8(num: u8) -> String {
-    format!("You entered the u8 number: {}", num)
-}
-
-#[get("/num/<num>", rank = 2)]
-fn num_u128(num: u128) -> String {
-    format!("You entered the u128 number: {}", num)
-}
-
 pub fn build_rocket() -> Rocket<rocket::Build> {
-    rocket::build().mount("/", routes![index, num_u8, num_u128])
+    rocket::build()
+        .mount("/", openapi_get_routes![index, test_enum, test_struct])
+        .mount(
+            "/swagger",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_string(),
+                deep_linking: true,
+                ..Default::default()
+            }),
+        )
 }
