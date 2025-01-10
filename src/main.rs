46bf781de671b0c873f34d5cadf7977c1fba4e2f
Torben Schweren <torben@schweren.dev>
2025-01-11 00:41:45 +0100

    WAIFU-9: refactor: start rocket manually instead of using launch macro

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use anyhow::Result;
 use rasopus::build_rocket;
-use rocket::{launch, Rocket};
 
-#[launch]
-fn rocket() -> Rocket<rocket::Build> {
-    build_rocket()
+#[rocket::main]
+async fn main() -> Result<()> {
+    println!("Starting Rocket");
+    let rocket = build_rocket();
+    let result = rocket.launch().await;
+
+    if let Err(e) = result {
+        eprintln!("Rocket had a runtime error: {}", e);
+        return Err(e.into());
+    }
+
+    Ok(())
 }
