fb29f084044f023e51156663efc2641e100bb533
Torben Schweren <torben@schweren.dev>
2025-01-15 00:00:24 +0100

    Add SQLx (#12)
    * WAIFU-9: refactor: copy only needed files into docker build stage
    
    * WAIFU-9: add: anyhow crate
    
    * WAIFU-9: refactor: start rocket manually instead of using launch macro
    
    * WAIFU-9: add: Rasopus environment options
    
    * WAIFU-9: refactor: configs rework
    
    * WAIFU-9: add: sqlx crate
    
    * WAIFU-9: add: SQLx AnyPool connection
    
    * WAIFU-9: add: database initialization
    
    * WAIFU-9: refactor: move code from main into lib
    
    * WAIFU-9: add: newlines to make match statements more readable
    
    * WAIFU-9: add: pass ownership of the database pool to rocket
    
    * add: empty first migration

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,29 @@
+use config::{database::DatabaseConfig, rasopus::RasopusConfig, rocket_overrides::RocketOverrides};
+use lum_config::{EnvHandler, EnvironmentConfigParseError};
 use rocket::Rocket;
 use rocket_okapi::swagger_ui::*;
+use sqlx::any::AnyPoolOptions;
+use thiserror::Error;
 
+pub mod config;
 pub mod controller;
+pub mod database;
 pub mod macros;
 
-pub fn build_rocket() -> Rocket<rocket::Build> {
-    rocket::build()
+pub fn parse_config_from_env<IntoString: Into<String>>(
+    app_name: IntoString,
+) -> Result<RasopusConfig, EnvironmentConfigParseError> {
+    EnvHandler::new(app_name).load_config()
+}
+
+pub fn build_rocket(
+    rocket_overrides: RocketOverrides,
+    managed_data: Vec<Box<dyn Send + Sync + 'static>>,
+) -> Rocket<rocket::Build> {
+    let mut figment = rocket::Config::figment();
+    figment = rocket_overrides.apply(figment);
+
+    let mut rocket = rocket::custom(figment)
         .mount("/", controller::openapi_get_routes())
         .mount(
             "/swagger",
@@ -14,5 +32,63 @@ pub fn build_rocket() -> Rocket<rocket::Build> {
                 deep_linking: true,
                 ..Default::default()
             }),
-        )
+        );
+
+    for data in managed_data {
+        rocket = rocket.manage(data);
+    }
+
+    rocket
+}
+
+#[derive(Debug, Error)]
+pub enum RuntimeError {
+    #[error("Failed to connect to database: {0}")]
+    DatabaseConnect(#[from] sqlx::Error),
+
+    #[error("Failed to check database migrations: {0}")]
+    CheckMigration(#[from] database::CheckMigrationError),
+
+    #[error("Failed to run database migrations: {0}")]
+    Migrate(#[from] sqlx::migrate::MigrateError),
+
+    #[error("Failed to start Rocket: {0}")]
+    Rocket(#[from] rocket::Error),
+}
+
+pub async fn run(rasopus_config: RasopusConfig) -> Result<(), RuntimeError> {
+    println!("Initializing database drivers");
+    sqlx::any::install_default_drivers();
+
+    println!("Connecting to database");
+    let database_config = DatabaseConfig::from(&rasopus_config);
+    let database_pool = AnyPoolOptions::new()
+        .max_connections(database_config.pool_size)
+        .connect(&database_config.to_connection_string())
+        .await?;
+
+    println!("Checking database migrations");
+    let migrator = sqlx::migrate!("./migrations");
+    let needs_migration = database::needs_migration(&database_pool, &migrator).await?;
+    if needs_migration {
+        println!("Applying missing database migrations");
+        migrator.run(&database_pool).await?;
+        println!("Database migrations applied");
+    } else {
+        println!("Database is up to date");
+    }
+
+    println!("Building Rocket with Rasopus configuration");
+    let rocket_overrides = RocketOverrides::from(&rasopus_config);
+    let rocket = build_rocket(rocket_overrides, vec![Box::new(database_pool)]);
+
+    println!("Starting Rocket");
+    let result = rocket.launch().await;
+
+    if let Err(e) = result {
+        eprintln!("Rocket had a runtime error: {}", e);
+        return Err(e.into());
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use rasopus::build_rocket;
-use rocket::{launch, Rocket};
+use anyhow::Result;
+use rasopus::{parse_config_from_env, run};
 
-#[launch]
-fn rocket() -> Rocket<rocket::Build> {
-    build_rocket()
+pub static APP_NAME: &str = "Rasopus";
+pub static APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[rocket::main]
+async fn main() -> Result<()> {
+    println!("Starting {} v{}", APP_NAME, APP_VERSION);
+
+    println!("Loading Rasopus environment variables");
+    let rasopus_config = match parse_config_from_env(APP_NAME) {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("Failed to load Rasopus environment variables: {}", e);
+            return Err(e.into());
+        }
+    };
+
+    let result = run(rasopus_config).await;
+    if let Err(e) = result {
+        eprintln!("Rasopus had a runtime error: {}", e);
+        return Err(e.into());
+    }
+
+    Ok(())
 }
