2e69114caab52d7a36311f68013b22d094df996a
Torben Schweren <torben@schweren.dev>
2025-01-16 21:44:12 +0100

    WAIFU-28: implement user model

--- migrations/20250114223920_init_database.sql
+++ migrations/20250114223920_init_database.sql
@@ -1,1 +1,7 @@
--- Add migration script here
+CREATE TABLE users (
+    uuid CHAR(36) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    role INTEGER NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at BIGINT NOT NULL
+);
